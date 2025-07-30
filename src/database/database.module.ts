14f52f79d32ab53def17e9749bb95309ba4732db
harunaabaldeh <harunaabaldeh@gmail.com>
2025-07-30 01:25:08 +0000

    feat(database): add DatabaseModule with TypeORM configuration for PostgreSQL

---
+++ src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class DatabaseModule {}
