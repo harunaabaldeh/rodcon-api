50a6e4706a3853fab0d19cfba543fd3151812952
harunaabaldeh <harunaabaldeh@gmail.com>
2025-07-30 01:32:04 +0000

    feat(user): add User entity with registry dates embedded

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,23 @@
+import { RegistryDates } from 'src/common/embedded/registry-dates.embedded';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   email: string;
+
+  @Column()
   phone: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
